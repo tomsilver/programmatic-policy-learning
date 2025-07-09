6564bfa7f308350279727882e6aec87349cb7ce5
Tom Silver <tomssilver@gmail.com>
2025-07-09 17:51:24 -0400

    add random actions approach

---
+++ tests/approaches/test_random_actions.py
@@ -0,0 +1,18 @@
+"""Tests for random_actions.py."""
+
+import gymnasium
+
+from programmatic_policy_learning.approaches.random_actions import RandomActionsApproach
+
+
+def test_random_actions_approach():
+    """Tests for RandomActionsApproach()."""
+    # Just test that this runs without crashing.
+    env = gymnasium.make("LunarLander-v3")
+    approach = RandomActionsApproach(env.action_space, seed=123)
+    obs, info = env.reset()
+    approach.reset(obs, info)
+    for _ in range(5):
+        action = approach.step()
+        obs, reward, terminated, _, info = env.step(action)
+        approach.update(obs, reward, terminated, info)
