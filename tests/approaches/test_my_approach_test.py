184a56085b77d82b510f451aa0bd287711017c90
Sruthi Sankararaman <ss5328@princeton.edu>
2025-09-18 13:13:07 -0400

    apply formatting fixes

--- tests/approaches/test_my_approach_test.py
+++ tests/approaches/test_my_approach_test.py
@@ -9,9 +9,7 @@ def test_same_action_approach():
     """Tests for RandomActionsApproach()."""
     # Just test that this runs without crashing.
     env = gymnasium.make("LunarLander-v3")
-    approach = MyApproachTest(
-        "N/A", env.observation_space, env.action_space, seed=123
-    )
+    approach = MyApproachTest("N/A", env.observation_space, env.action_space, seed=123)
     obs, info = env.reset()
     approach.reset(obs, info)
     for _ in range(5):
