8936869130e73d2846689eb813aee11e38644fbf
Zahra <zbashir1@ualberta.ca>
2025-09-18 10:34:58 -0600

    feat: add test for gym_to_gymnasium and wrappers

---
+++ tests/env/test_wrappers.py
@@ -0,0 +1,10 @@
+"""Tests for wrappers."""
+from gymnasium import spaces
+from programmatic_policy_learning.envs.utils.wrappers import patch_box_float32
+
+def test_patch_box_float32() -> None:
+    """Test patch_box_float32 changes Box dtype."""
+    original_box_init = patch_box_float32()
+    box = spaces.Box(low=0, high=1, shape=(2,), dtype=float)
+    assert box.dtype == spaces.Box(low=0, high=1, shape=(2,), dtype=float).dtype
+    spaces.Box.__init__ = original_box_init
