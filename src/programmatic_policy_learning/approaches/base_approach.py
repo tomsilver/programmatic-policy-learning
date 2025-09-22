401c0c44bc9a75946680e514d5f35b66b3118d81
Zahra <zbashir1@ualberta.ca>
2025-09-21 18:04:58 -0600

    Change BaseApproach based on the needs for LPP

--- src/programmatic_policy_learning/approaches/base_approach.py
+++ src/programmatic_policy_learning/approaches/base_approach.py
@@ -26,3 +26,30 @@ class BaseApproach(Agent[_ObsType, _ActType], Generic[_ObsType, _ActType], abc.A
         self._action_space.seed(seed)
         self._observation_space = observation_space
         self._observation_space.seed(seed)
+        self._is_trained = False
+
+    def fit(  # pylint: disable=unused-argument
+        self, demonstrations: list[list[tuple[_ObsType, _ActType]]]
+    ) -> None:
+        """Train the approach on demonstration data.
+
+        Args:
+            demonstrations: List of episodes,
+            each episode is a list of (state, action) pairs
+
+        Note: Default no-op implementation. Override if training is needed.
+        """
+        self._is_trained = True
+
+    def save(self, filepath: str) -> None:
+        """Save the trained model to disk."""
+        raise NotImplementedError("Save method not implemented")
+
+    def load(self, filepath: str) -> None:
+        """Load a trained model from disk."""
+        raise NotImplementedError("Load method not implemented")
+
+    @property
+    def is_trained(self) -> bool:
+        """Whether the approach has been trained."""
+        return self._is_trained
