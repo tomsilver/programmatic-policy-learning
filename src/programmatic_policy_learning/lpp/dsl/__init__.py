5394207186e2bb0ad47ee98f5f4c4ede33bdfe1b
Zahra <zbashir1@ualberta.ca>
2025-09-23 11:26:11 -0600

    Fix the absolute import

--- src/programmatic_policy_learning/lpp/dsl/__init__.py
+++ src/programmatic_policy_learning/lpp/dsl/__init__.py
@@ -1,5 +1,5 @@
 """DSL primitives for LPP programs."""
 
-from .primitives import cell_is_value, out_of_bounds
+from programmatic_policy_learning.lpp.dsl.primitives import cell_is_value, out_of_bounds
 
 __all__ = ["cell_is_value", "out_of_bounds"]
