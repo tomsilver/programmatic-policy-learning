1b62c7d62ea968b01a6447280c51183c68ff0bbf
Zahra Bashir <zbashir1@ualberta.ca>
2025-09-23 18:23:58 -0600

    Full list of primitives added + tests (#16)
    * Change BaseApproach based on the needs for LPP
    
    * Add policies directory, including lpp_policy.py
    
    * Add grammar and dsl logic
    
    * Add demonstration logic
    
    * Remove all the added files for LPP
    
    * Add minimal LPP implementation with primitives and StateActionProgram
    
    * Fix type annotations
    
    * Remove classes from tests
    
    * Fix the absolute import
    
    * Make the StateActionProgram class more generic
    
    * Add tests for eval_context addition to StateActionProgram
    
    * Change Dict to dict
    
    * Add all primitives
    
    * Apply a few fixes

--- src/programmatic_policy_learning/lpp/dsl/__init__.py
+++ src/programmatic_policy_learning/lpp/dsl/__init__.py
@@ -1,5 +1,19 @@
 """DSL primitives for LPP programs."""
 
-from programmatic_policy_learning.lpp.dsl.primitives import cell_is_value, out_of_bounds
+from programmatic_policy_learning.lpp.dsl.primitives import (
+    at_action_cell,
+    at_cell_with_value,
+    cell_is_value,
+    out_of_bounds,
+    scanning,
+    shifted,
+)
 
-__all__ = ["cell_is_value", "out_of_bounds"]
+__all__ = [
+    "cell_is_value",
+    "out_of_bounds",
+    "shifted",
+    "at_cell_with_value",
+    "at_action_cell",
+    "scanning",
+]
--- tests/conftest.py
+++ tests/conftest.py
@@ -3,8 +3,10 @@
 See https://docs.pytest.org/en/6.2.x/fixture.html.
 """
 
+import pytest
 
-def pytest_addoption(parser):
+
+def pytest_addoption(parser: pytest.Parser) -> None:
     """Enable a command line flag for running tests decorated with @runllms."""
     parser.addoption(
         "--runllms",
