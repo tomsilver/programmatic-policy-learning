65fba0a8e6dad73415e02fb504732690e4b2a5c8
Zahra <zbashir1@ualberta.ca>
2025-09-29 16:03:13 -0600

    Add a test with input variables (x)

--- tests/dsl/generators/test_grammar_based_generator.py
+++ tests/dsl/generators/test_grammar_based_generator.py
@@ -56,3 +56,43 @@ def test_grammar_based_program_generator():
         generated_programs.append(next(gen))
 
     assert expected_programs == generated_programs
+
+
+def test_grammar_based_program_generator_with_input():
+    """Test GrammarBasedProgramGenerator with external input variable."""
+
+    def add_one(x: int) -> int:
+        return x + 1
+
+    prims = {
+        "add_one": add_one,
+    }
+
+    # Eval function expects 'program' as a string with 'x' as a variable,
+    # and 'inputs' as the value for 'x'.
+    def _eval(program: str, inputs: int):
+        return eval(program, {}, {"x": inputs, **prims})
+
+    dsl = DSL(id="dummy", primitives=prims, evaluate_fn=_eval)
+
+    INT, INCREMENT = 0, 1
+
+    def _create_grammar(env_spec):
+        del env_spec
+        grammar: Grammar[str, int, int] = Grammar(
+            rules={
+                INT: ([["x"]], [1.0]),
+                INCREMENT: ([["add_one(", INT, ")"]], [1.0]),
+            }
+        )
+        return grammar
+
+    program_generator = GrammarBasedProgramGenerator(
+        _create_grammar, dsl, env_spec={}, start_symbol=INCREMENT
+    )
+
+    gen = program_generator.generate_programs()
+    program = next(gen)  # 'add_one( x )'
+
+    assert dsl.evaluate(program, 0) == 1
+    assert dsl.evaluate(program, 5) == 6
