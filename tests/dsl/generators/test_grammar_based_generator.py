341a5f98664ebd761027dea0ff0ef1d62581e8fd
Zahra <zbashir1@ualberta.ca>
2025-09-29 17:32:40 -0600

    Add test + final adjustments

--- tests/dsl/generators/test_grammar_based_generator.py
+++ tests/dsl/generators/test_grammar_based_generator.py
@@ -1,10 +1,14 @@
 """Tests for grammar_based_generator.py."""
 
+from omegaconf import OmegaConf
+
 from programmatic_policy_learning.dsl.core import DSL
 from programmatic_policy_learning.dsl.generators.grammar_based_generator import (
     Grammar,
     GrammarBasedProgramGenerator,
 )
+from programmatic_policy_learning.dsl.primitives_sets.grid_v1 import create_grammar
+from programmatic_policy_learning.envs.providers.ggg_provider import create_ggg_env
 
 
 def test_grammar_based_program_generator():
@@ -96,3 +100,23 @@ def test_grammar_based_program_generator_with_input():
 
     assert dsl.evaluate(program, 0) == 1
     assert dsl.evaluate(program, 5) == 6
+
+
+def test_grid_grammar_infers_env_spec_from_env():
+    """Test that env_spec is inferred from GGGEnvWithTypes and used in
+    grammar."""
+    cfg = OmegaConf.create({"make_kwargs": {"id": "TwoPileNim0-v0"}})
+    env = create_ggg_env(cfg)
+    object_types = env.get_object_types()
+    env_spec = {"object_types": object_types}
+    dsl: DSL[str, None, str] = DSL(
+        id="grid_v1", primitives={}, evaluate_fn=lambda p, i: p
+    )
+    generator = GrammarBasedProgramGenerator(
+        create_grammar, dsl, env_spec=env_spec, start_symbol=6  # 6 is VALUE
+    )
+    gen = generator.generate_programs()
+
+    assert next(gen) == "tpn.EMPTY"
+    assert next(gen) == "tpn.TOKEN"
+    assert next(gen) == "None"
