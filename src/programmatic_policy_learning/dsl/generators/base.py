6773e3f19d6ffe2b94b64fe4552df8776a015aad
Zahra Bashir <zbashir1@ualberta.ca>
2025-09-29 18:03:24 -0600

    Add program generators (#25)
    * program generators
    
    * tests pass but messy
    
    * wip
    
    * Fix type-annotations and minor issues
    
    * Add a test with input variables (x)
    
    * Add ggg grammar + env class that contains getType
    
    * Add test + final adjustments
    
    ---------
    
    Co-authored-by: Tom Silver <tomssilver@gmail.com>

---
+++ src/programmatic_policy_learning/dsl/generators/base.py
@@ -0,0 +1,30 @@
+"""Base class for program generators."""
+
+import abc
+from typing import Any, Generic, Iterator, TypeVar
+
+from programmatic_policy_learning.dsl.core import DSL
+
+ProgT = TypeVar("ProgT")
+InT = TypeVar("InT")
+OutT = TypeVar("OutT")
+
+
+class ProgramGenerator(Generic[ProgT, InT, OutT], abc.ABC):
+    """Base class for program generators."""
+
+    def __init__(self, dsl: DSL[ProgT, InT, OutT], env_spec: dict[str, Any]) -> None:
+        self._dsl = dsl
+        # NOTE: env specs can be used, for example, to extract possible object types
+        # in GGG environments which are included as part of primitive programs.
+        self._env_spec = env_spec
+
+    @abc.abstractmethod
+    def generate_programs(
+        self,
+    ) -> Iterator[str]:  # changed to str to match stringify output
+        """Generate a potentially infinite number of programs from the DSL.
+
+        Typically, program generators should go from "simplest" to "most
+        complex".
+        """
