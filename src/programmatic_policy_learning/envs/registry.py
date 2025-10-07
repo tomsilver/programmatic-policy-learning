83f3302807fd535c56852567b3a91dbc2bf9bd02
Muhammad Zaeem <zaeemmuhammad555@gmail.com>
2025-10-07 08:30:44 -0400

    add custom maze to registry

--- src/programmatic_policy_learning/envs/registry.py
+++ src/programmatic_policy_learning/envs/registry.py
@@ -8,9 +8,8 @@ from programmatic_policy_learning.envs.providers.ggg_provider import create_ggg_
 from programmatic_policy_learning.envs.providers.prbench_provider import (
     create_prbench_env,
 )
-from programmatic_policy_learning.envs.providers.maze_provider import create_maze_env
-from programmatic_policy_learning.envs.providers.outer_maze_provider import (
-    create_outer_maze_env,
+from programmatic_policy_learning.envs.providers.maze_provider import (
+    create_maze_env,
 )
 
 class EnvRegistry:
@@ -20,8 +19,7 @@ class EnvRegistry:
         self._providers: dict[str, Callable[[Any], Any]] = {
             "ggg": create_ggg_env,
             "prbench": create_prbench_env,
-            "gym_maze": create_maze_env,
-            "outer_maze": create_outer_maze_env,
+            "maze": create_maze_env,
         }
 
     def load(self, env_config: Any) -> Any:
