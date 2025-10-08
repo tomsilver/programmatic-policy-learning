c1b463ff538db1330132d7528577e93ffbbf0914
Zahra Bashir <zbashir1@ualberta.ca>
2025-10-08 16:06:57 -0600

    Dataset - get_demonstrations (#39)
    * Add dataset file
    
    * Change mypy sensitivity and fix many files annotations
    
    * Add tests for dataset.py
    
    * Add pipline test for dataset-creation
    
    * Fix demo failure
    
    * Modify Trajectory dataclass type and apply changes to dataset.py
    
    * Return log_priors
    
    * Fix types
    
    * Add StateActionProgram class
    
    * Add test for StateActionProgram
    
    * Pass dsl_functions to StateActionProgram
    
    * Apply a minor change
    
    * Add get_demonstrations function (list of traj)
    
    * Add test for get_demonstrations

--- tests/data/test_collect.py
+++ tests/data/test_collect.py
@@ -8,46 +8,48 @@ from omegaconf import DictConfig, OmegaConf
 from programmatic_policy_learning.approaches.expert_approach import ExpertApproach
 from programmatic_policy_learning.approaches.experts.grid_experts import get_grid_expert
 from programmatic_policy_learning.approaches.random_actions import RandomActionsApproach
-from programmatic_policy_learning.data.collect import collect_demo
+from programmatic_policy_learning.data.collect import collect_demo, get_demonstrations
 from programmatic_policy_learning.data.demo_types import Trajectory
 from programmatic_policy_learning.envs.registry import EnvRegistry
+
+
+class DummySpace:
+    """DummySpace."""
+
+    def sample(self) -> int:
+        """Sample."""
+        return 0
+
+    def seed(self, _: int) -> None:
+        """Seed."""
+        pass  # pylint: disable=unnecessary-pass
+
+
+class DummyEnv:
+    """DummyEnv."""
+
+    def reset(self) -> tuple[np.ndarray, dict]:
+        """Reset."""
+        return np.zeros((2, 2)), {}
+
+    def step(self, _: int) -> tuple[np.ndarray, float, bool, bool, dict]:
+        """Step."""
+        return np.zeros((2, 2)), 1.0, True, False, {}
+
+    @property
+    def observation_space(self) -> DummySpace:
+        """Observation space."""
+        return DummySpace()
+
+    @property
+    def action_space(self) -> DummySpace:
+        """Action space."""
+        return DummySpace()
 
 
 def test_collect_demo_returns_trajectory_DummyEnv() -> None:
     """Test that collect_demo returns a Trajectory with Demo steps,
     DummyEnv."""
-
-    class DummySpace:
-        """DummySpace."""
-
-        def sample(self) -> int:
-            """Sample."""
-            return 0
-
-        def seed(self, _: int) -> None:
-            """Seed."""
-            pass  # pylint: disable=unnecessary-pass
-
-    class DummyEnv:
-        """DummyEnv."""
-
-        def reset(self) -> tuple[np.ndarray, dict]:
-            """Reset."""
-            return np.zeros((2, 2)), {}
-
-        def step(self, _: int) -> tuple[np.ndarray, float, bool, bool, dict]:
-            """Step."""
-            return np.zeros((2, 2)), 1.0, True, False, {}
-
-        @property
-        def observation_space(self) -> DummySpace:
-            """Observation space."""
-            return DummySpace()
-
-        @property
-        def action_space(self) -> DummySpace:
-            """Action space."""
-            return DummySpace()
 
     env = DummyEnv()
     env_factory = lambda: env  # returns a new environment each time you call
@@ -110,3 +112,29 @@ def test_collect_demo_with_real_env_and_expert() -> None:
     assert isinstance(traj.steps, list)
     assert isinstance(traj.steps[0], tuple)
     assert len(traj.steps) > 0
+
+
+def test_get_demonstrations() -> None:
+    """Test get_demonstrations collects multiple trajectories."""
+    env = DummyEnv()
+    env_factory = lambda: env
+
+    expert: RandomActionsApproach = RandomActionsApproach(
+        "TEST",
+        env.observation_space,  # type: ignore
+        env.action_space,  # type: ignore
+        seed=1,
+    )
+
+    demo_numbers = (1, 2, 3)
+
+    demonstrations = get_demonstrations(
+        env_factory, expert, demo_numbers=demo_numbers, max_demo_length=5
+    )
+    assert isinstance(demonstrations, list)
+    assert len(demonstrations) == len(demo_numbers)
+    for traj in demonstrations:
+        assert isinstance(traj, Trajectory)
+        assert isinstance(traj.steps, list)
+        assert len(traj.steps) > 0
+        assert isinstance(traj.steps[0], tuple)
