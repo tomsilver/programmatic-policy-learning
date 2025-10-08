93cb58ea86c855d231f927386040c10886165697
Zahra <zbashir1@ualberta.ca>
2025-10-08 14:02:42 -0600

    Update LPPPolicy class

--- src/programmatic_policy_learning/approaches/lpp_approach.py
+++ src/programmatic_policy_learning/approaches/lpp_approach.py
@@ -1,23 +1,34 @@
 """An approach that learns a logical programmatic policy from data."""
-import numpy as np
-from typing import Any, TypeVar
 
+from typing import Any, Callable, TypeVar, cast
+
+import numpy as np
 from gymnasium.spaces import Space
 from scipy.special import logsumexp
 
 from programmatic_policy_learning.approaches.base_approach import BaseApproach
-from programmatic_policy_learning.dsl.state_action_program import StateActionProgram
-from programmatic_policy_learning.policies.lpp_policy import LPPPolicy
-from programmatic_policy_learning.learning.decision_tree_learner import learn_plps
-from programmatic_policy_learning.learning.plp_likelihood import compute_likelihood_plps
-from programmatic_policy_learning.learning.particles_utils import select_particles
-from programmatic_policy_learning.dsl.generators.grammar_based_generator import GrammarBasedProgramGenerator
-from programmatic_policy_learning.dsl.primitives_sets.grid_v1 import create_grammar, make_dsl
 from programmatic_policy_learning.data.collect import collect_demo
-from programmatic_policy_learning.data.demo_types import Trajectory
 from programmatic_policy_learning.data.dataset import run_all_programs_on_demonstrations
+from programmatic_policy_learning.data.demo_types import Trajectory
+from programmatic_policy_learning.dsl.generators.grammar_based_generator import (
+    Grammar,
+    GrammarBasedProgramGenerator,
+)
+from programmatic_policy_learning.dsl.primitives_sets.grid_v1 import (
+    GridInput,
+    LocalProgram,
+    create_grammar,
+    make_dsl,
+)
+from programmatic_policy_learning.dsl.state_action_program import StateActionProgram
+from programmatic_policy_learning.learning.decision_tree_learner import learn_plps
+from programmatic_policy_learning.learning.particles_utils import select_particles
+from programmatic_policy_learning.learning.plp_likelihood import compute_likelihood_plps
+from programmatic_policy_learning.policies.lpp_policy import LPPPolicy
+
 _ObsType = TypeVar("_ObsType")
 _ActType = TypeVar("_ActType")
+EnvFactory = Callable[[], Any]
 
 
 class LogicProgrammaticPolicyApproach(BaseApproach[_ObsType, _ActType]):
@@ -29,24 +40,30 @@ class LogicProgrammaticPolicyApproach(BaseApproach[_ObsType, _ActType]):
         observation_space: Space[_ObsType],
         action_space: Space[_ActType],
         seed: int,
+        env_factory: EnvFactory,
+        expert: BaseApproach,
         base_class_name: str = "",
         demo_numbers: list[int] = [],
         program_generation_step_size: int = 10,
         num_programs: int = 100,
         num_dts: int = 5,
         max_num_particles: int = 10,
-        env_specs: dict[str, Any]=None,
+        max_demo_length: int | float = np.inf,
+        env_specs: dict[str, Any] = {},
         start_symbol: int = 0,
     ) -> None:
         """LPP APProach."""
         super().__init__(environment_description, observation_space, action_space, seed)
         self._policy: LPPPolicy | None = None
+        self.env_factory = env_factory
+        self.expert = expert
         self.base_class_name = base_class_name
         self.demo_numbers = demo_numbers
         self.program_generation_step_size = program_generation_step_size
         self.num_programs = num_programs
         self.num_dts = num_dts
         self.max_num_particles = max_num_particles
+        self.max_demo_length = max_demo_length
         self.env_specs = env_specs
         self.start_symbol = start_symbol
 
@@ -59,7 +76,13 @@ class LogicProgrammaticPolicyApproach(BaseApproach[_ObsType, _ActType]):
         """Train the logical programmatic policy using demonstrations."""
         dsl = make_dsl()
         program_generator = GrammarBasedProgramGenerator(
-            create_grammar, dsl, env_spec=self.env_specs, start_symbol= self.start_symbol
+            cast(
+                Callable[[dict[str, Any]], Grammar[LocalProgram, GridInput, Any]],
+                create_grammar,
+            ),
+            dsl,
+            env_spec=self.env_specs,
+            start_symbol=self.start_symbol,
         )
 
         # Generate programs and their priors
@@ -71,15 +94,22 @@ class LogicProgrammaticPolicyApproach(BaseApproach[_ObsType, _ActType]):
             programs.append(program)
             program_prior_log_probs.append(prior)
 
-        
-        # demonstrations = get_demonstrations(env_factory, expert, demo_numbers=self.demo_numbers) 
         # uncomment after the PR is approved on dataset branch
-        
-        X, y = run_all_programs_on_demonstrations(self.base_class_name, self.demo_numbers,programs, demonstrations)
+        # demonstrations = get_demonstrations(self.env_factory, expert, demo_numbers=self.demo_numbers)
+        demonstrations: list[Trajectory[np.ndarray, tuple[int, int]]] = [
+            collect_demo(self.env_factory, self.expert, self.max_demo_length)
+        ]
+
+        X, y = run_all_programs_on_demonstrations(
+            self.base_class_name, self.demo_numbers, programs, demonstrations
+        )
         plps, plp_priors = learn_plps(
-            X, y, programs, program_prior_log_probs,
+            X,
+            y,
+            programs,
+            program_prior_log_probs,
             num_dts=self.num_dts,
-            program_generation_step_size=self.program_generation_step_size
+            program_generation_step_size=self.program_generation_step_size,
         )
         likelihoods = compute_likelihood_plps(plps, demonstrations)
 
@@ -93,9 +123,13 @@ class LogicProgrammaticPolicyApproach(BaseApproach[_ObsType, _ActType]):
         print(f"MAP program ({particle_log_probs[map_idx]}):")
         print(particles[map_idx])
 
-        top_particles, top_particle_log_probs = select_particles(particles, particle_log_probs, self.max_num_particles)
+        top_particles, top_particle_log_probs = select_particles(
+            particles, particle_log_probs, self.max_num_particles
+        )
         if len(top_particle_log_probs) > 0:
-            top_particle_log_probs = np.array(top_particle_log_probs) - logsumexp(top_particle_log_probs)
+            top_particle_log_probs = np.array(top_particle_log_probs) - logsumexp(
+                top_particle_log_probs
+            )
             top_particle_probs = np.exp(top_particle_log_probs)
             print("top_particle_probs:", top_particle_probs)
             policy = LPPPolicy(top_particles, top_particle_probs)
@@ -104,7 +138,7 @@ class LogicProgrammaticPolicyApproach(BaseApproach[_ObsType, _ActType]):
             policy = LPPPolicy([StateActionProgram("False")], [1.0])
 
         return policy
-    
+
     def _get_action(self) -> _ActType:
         assert self._policy is not None, "Call reset() first."
         assert self._last_observation is not None
