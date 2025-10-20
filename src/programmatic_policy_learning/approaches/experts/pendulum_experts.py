03f6e312e493f7099c630db3e11a5841adc7d963
Tom Silver <tomssilver@gmail.com>
2025-10-20 13:18:54 -0400

    wip

--- src/programmatic_policy_learning/approaches/experts/pendulum_experts.py
+++ src/programmatic_policy_learning/approaches/experts/pendulum_experts.py
@@ -1,10 +1,12 @@
 """Expert policies for the pendulum environment (may not be optimal!)."""
 
-from typing import Callable
+from typing import Any, Callable
 
 import gymnasium as gym
 import numpy as np
 from numpy.typing import NDArray
+
+from programmatic_policy_learning.approaches.structs import ParametricPolicyBase
 
 Obs = NDArray[np.float32]
 Act = NDArray[np.float32]
@@ -55,3 +57,41 @@ def create_manual_pendulum_policy(action_space: gym.spaces.Box) -> Callable[[Obs
         return np.array([torque], dtype=action_space.dtype)
 
     return manual_pendulum_policy
+
+
+class PendulumParametricPolicy(ParametricPolicyBase):
+    """An "expert" parametric policy for the pendulum environment."""
+
+    def __init__(
+        self,
+        *args: dict[str, Any],
+        min_torque: float = -2.0,
+        max_torque: float = 2.0,
+        **kwargs: dict[str, Any],
+    ) -> None:
+        super().__init__(*args, **kwargs)
+        self._min_torque = min_torque
+        self._max_torque = max_torque
+
+    def act(self, obs: Any) -> Any:
+        # Extract the parameters.
+        kp = self._params["kp"]
+        kd = self._params["kd"]
+
+        # Main logic.
+        assert isinstance(obs, np.ndarray)
+        x, y, angvel = float(obs[0]), float(obs[1]), float(obs[2])
+        theta = float(np.arctan2(y, x))
+
+        near_top = abs(theta) < 0.5
+        if near_top:
+            torque = -kp * theta - kd * angvel
+        else:
+            torque = self._max_torque if angvel >= 0.0 else self._min_torque
+
+        # Clip.
+        torque = np.clip(torque, self._min_torque, self._max_torque)
+
+        # Return the action as a numpy arary.
+        act = np.array([torque], dtype=np.float32)
+        return act
